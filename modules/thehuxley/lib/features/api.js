9929f86919f355ede994298b6376fdd4b0b1d24a
Lucas <lbam@cin.ufpe.br>
2018-03-08 17:02:26 -0300

    Finish basic the huxley module

--- modules/thehuxley/lib/features/api.js
+++ modules/thehuxley/lib/features/api.js
@@ -1,48 +1,81 @@
 const axios = require("axios");
-const config = require("../../../../config/constants.js");
-var authorization;
+const config = require(__base + "config/constants.js");
+const _ = require("lodash");
+const moment = require("moment")
+var authorization = null;
+axios.defaults.baseURL = "https://www.thehuxley.com/api";
 
-async function getListas() {
-    await login();
-    // [{"id":2142,
-    // "title":"Lista 1 - 2018.1 (Pokémon)",
-    // "description":null,"score":15.0,
-    // "startDate":"2018-03-07T10:00:00-03:00",
-    // "endDate":"2018-03-14T23:59:00-03:00",
-    // "serverTime":"2018-03-08T15:51:25-03:00",
-    // "dateCreated":"2017-08-16T16:52:58-03:00",
-    // "lastUpdated":"2018-03-01T14:10:34-03:00",
-    // "group":{
-    //     "id":194,
-    //     "name":"IP-CC-CIn-UFPE",
-    //     "url":"ipcc-cin-ufpe",
-    //     "description":"Introdução à Programação - Ciência da Computação - Universidade Federal de Pernambuco",
-    //     "startDate":"2017-08-07T00:00:00-03:00",
-    //     "endDate":"2017-11-30T00:00:00-03:00",
-    //     "dateCreated":"2015-09-01T11:21:50-03:00",
-    //     "lastUpdated":"2018-03-01T11:20:36-03:00",
-    //     "institution":{
-    //         "id":48,"name":"Universidade Federal de Pernambuco",
-    //         "acronym":"UFPE",
-    //         "logo":"https://www.thehuxley.com/api/v1/institutions/logo/default.png",
-    //         "status":"APPROVED"
-    //     }
-    // },
-    // "partialScore":false
-    // }
+//obs: O JSON retornado pelas requisições estarão na variável .data da requisição do axios.
+//ex: getSubmissionCode(1055140).then((response) => (console.log(response.data)));
+
+async function getSubmissionCode(submissionID){
+    try {
+        await login();
+        return axios.get(`https://www.thehuxley.com/api/v1/submissions/${submissionID}/sourcecode`);
+    } catch(err){
+        return Promise.reject(err);
+    }
+}
+
+async function getStudentSubmissions(problemID, userID){
+    try {
+        await login();
+        return axios.get(`v1/submissions?problem=${problemID}&user=${userID}`);
+    } catch(err){
+        return Promise.reject(err);
+    }
+}
+
+//pega as listas e filtra, deixando apenas as listas do ano atual e que já fecharam.
+async function getFilteredLists(){
+    try {
+        let lists = await getLists();
+        lists = _.filter(lists.data, function(obj) {
+            return moment().year() == moment(obj.endDate).year() && moment() >= moment(obj.endDate);
+        }) 
+        return Promise.resolve(lists);
+    } catch(err){
+        return Promise.reject(err);
+    }
+}
+
+async function getListProblems(listID){
+    try {
+        await login();
+        return axios.get(`/v1/quizzes/${listID}/problems?max=100&offset=0`);
+    } catch (err){
+        return Promise.reject(err);
+    }
+}
+
+async function getLists() {
+    try {
+        await login();
+        return axios.get("/v1/groups/194/quizzes?max=30&offset=0&order=desc&sort=startDate")
+    } catch (err) {
+        return Promise.reject(err);
+    }
 }
 
 async function login() {
     return new Promise((resolve, reject) => {
         if(!authorization || authorization.created_at + authorization.expires_in <= new Date().getTime()){
-            axios.post("https://www.thehuxley.com/api/login", {
+            axios.post("/login", {
                 username: config.thehuxley.username,
                 password: config.thehuxley.password
             }).then((response) => {
                 authorization = response.data;
                 authorization.created_at = new Date().getTime();
+                axios.defaults.headers.common['Authorization'] = "Bearer " + authorization.access_token;
                 resolve();
             }).catch((err) => reject(err));
         }
     })
+}
+
+module.exports = {
+    getSubmissionCode,
+    getStudentSubmissions,
+    getFilteredLists,
+    getListProblems
 }
