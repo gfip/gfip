e361b4b63b2e944e277ebb32b4372b88744997cb
Lucas <lbam@cin.ufpe.br>
2018-03-09 19:29:48 -0300

    Add isMonitor feature to authorization  module

--- modules/authorization/lib/features/isMonitor.js
+++ modules/authorization/lib/features/isMonitor.js
@@ -1,11 +1,30 @@
 const cheerio = require("cheerio");
-const URL = require("url-parse");
-
-/**
+const axios = require("axios");
+const URL = "https://sites.google.com/a/cin.ufpe.br/if669/"
+/** Check if user requesting to register is a monitor.
  * @param {string} username - User's CIn login
- */
-// Check if user requesting to register is a monitor.
-module.exports =  function(username) {
+ */ 
 
+//isMonitor('lbam').then((username) => console.log(username)).catch((err) => console.log(err.message))
+function isMonitor(username) {
+    return new Promise((resolve, reject) => {
+        axios.get(URL).then((body) => {
+            return cheerio.load(body.data);
+        })
+        .then(($) => {
+            //console.log($('.sites-layout-tile').children().first().children().last().children().last().children().last().children().first().children('b').text())
+            $('a').filter(function(i, elem) {
+                let link = $(elem).attr('href').split('.com')[0];
+                return link == 'https://i.imgur' || link == 'http://i.imgur';
+              }).each((function(i, elem){
+                  if($(elem).text() == username)
+                    resolve(username);
+              }))
 
+              reject(new Error('User trying to register is not a IP-CC-UFPE Monitor'));
+        })
+        .catch((err) => reject(err));
+    })
 }
+
+module.exports = isMonitor;
