8714ed88860093a1f03852ff2372bd8ec126df70
Lucas <lbam@cin.ufpe.br>
2018-03-09 18:05:22 -0300

    Remove react routes

--- routes/react.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
-module.exports = router;
