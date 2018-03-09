5168c7904f454762c8a67d166a99848597dbf45e
Rafael Mota <rma7@cin.ufpe.br>
2018-03-09 17:03:12 -0300

    substitute db to enviroment variable

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var studentRoutes = require("./routes/student.js");
 app.use(bodyParser.urlencoded({extended:true}));// body-parser
 app.use(methodOverride("_method"));// method-override
 
-mongoose.connect("mongodb://localhost/feedback-generator").catch((err) => console.log(err.message)); // connecting db
+mongoose.connect( process.env.MONGO_URL ).catch((err) => console.log(err.message)); // connecting db
 
 // setting passport
 app.use(passport.initialize());
