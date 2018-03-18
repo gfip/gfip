d93fcadfef0b02b5a2920425a6b9d63f4a9beb5a
Lucas <lbam@cin.ufpe.br>
2018-03-18 01:59:56 -0300

    Add login validation (temporary)

--- client/src/login/index.js
+++ client/src/login/index.js
@@ -1,30 +1,57 @@
 import React, { Component } from 'react';
 import logo from './img/logo.svg';
-import {Button, Form } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
+import { login } from '../helpers/api';
+import Cookies from 'universal-cookie';
 import './login.css';
 
 class LoginIndex extends Component {
-  handleSubmit(){
-    
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.username.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    login(this.state)
+    .then((response) => new Cookies().set('gfip_token', response.data.token))
+    .catch((err) => {
+        this.setState({status: err.response.data});
+    });
   }
 
   render() {
     return (
       <div id="main" className="container column centered">
           <div className="container column centered">
-          <img src={logo} id="logo" alt="logo" />
-          <Form onSubmit={this.handleSubmit} id="loginForm">
-            <Form.Field>
-              <input name="username" placeholder='Username'/>
-            </Form.Field>
-            <Form.Field>
-              <input type="password" name="password" placeholder='Password'/>
-            </Form.Field>
-            <div className="buttonHolder">
-              <Button type="submit" id="loginButton">Login</Button>
-            </div>
-          </Form>
-          <p>Doesn't have an account yet? <a id="registerLink" href="/register">Register here</a></p>
+            <img src={logo} id="logo" alt="logo" />
+            <Form onSubmit={this.handleSubmit} id="loginForm">
+              <Form.Field>
+                <input name="username" onChange={this.handleChange} placeholder='Username'/>
+              </Form.Field>
+              <Form.Field>
+                <input type="password" onChange={this.handleChange} name="password" placeholder='Password'/>
+              </Form.Field>
+              <span id="statusFeedback"> {this.state.status} </span>
+              <div className="buttonHolder">
+                <Button type="submit" id="loginButton">Login</Button>
+              </div>
+            </Form>
+            <p id="registerText">Doesn't have an account yet? <a id="registerLink" href="/register">Register here</a></p>
           </div>
       </div>
     );
