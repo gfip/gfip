28d5a6f9b57241bbe291cc8d9ce0d82c24fedf7a
Lucas <lbam@cin.ufpe.br>
2018-03-17 21:07:02 -0300

    Add login page

---
+++ client/src/notfound/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFoundIndex extends Component {
+  render() {
+    return (
+      <p> 404 NOT FOUND </p>
+    );
+  }
+}
+
+export default NotFoundIndex;
