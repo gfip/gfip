b1663769bf0313f5e31799318d78bbe59a22c234
Lucas <lbam@cin.ufpe.br>
2018-03-25 15:44:24 -0300

    Add log user

--- client/src/containers/NotFoundPage.js
+++ client/src/containers/NotFoundPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class NotFoundPage extends Component {
   render() {
+    console.log(this.props.user);
     return (
       <p> 404 NOT FOUND </p>
     );
