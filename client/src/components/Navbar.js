5a2eb062c9407ef4f0f4f0fb2080a0a15c40d64d
Lucas <lbam@cin.ufpe.br>
2018-04-29 22:19:01 -0300

    Add navbar menu, add logout feature

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import logo from '../assets/img/logo.svg';
+import MenuNavbar from './MenuNavbar';
+import OutsideDeactivator from './OutsideDeactivator';
 import '../assets/navbar.css';
 class Navbar extends Component {
   
     constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+        super(props);
+        this.state = {
+            menu: false
+        };
+    }
+    
+    openMenu = event => {
+        this.setState({menu: true})        
+    }
 
-  render() {
-      console.log(this.props.user.imageUrl);
-      return (
-        <div id="navbar" className="container verticalCentered spaceBetween">
-            <img src={logo} id="navbarLogo" alt="logo" />
-            <img src={this.props.user.imageUrl + ".jpg"} alt="userImage" id="userImage"/>
-        </div>
-      );
+    deactivatedMenu(){
+        this.setState({menu: false});
+    }
+
+    render() {
+        let MenuComponent = this.state.menu ? <OutsideDeactivator component={MenuNavbar} callback={this.deactivatedMenu.bind(this)}/> : null;
+        return (
+            <div id="navbar" className="container verticalCentered spaceBetween">
+                <img src={logo} id="navbarLogo" alt="logo" />
+                <img onClick={this.openMenu} src={this.props.user.imageUrl + ".jpg"} alt="userImage" id="userImage"/>
+                {MenuComponent}
+            </div>
+        );
   }
 }
 
