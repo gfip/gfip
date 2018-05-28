850f9ec4391583c15e6114efcde0288e49167f48
Lucas <lbam@cin.ufpe.br>
2018-05-28 20:08:39 -0300

    Add submissions label text

--- client/src/components/CodeViewer.js
+++ client/src/components/CodeViewer.js
@@ -17,7 +17,7 @@ class CodeViewer extends Component {
         return (
             <div className="container column centered report_main">
                 <div className='container row report_label'>
-                    {code && <Label as='a' color='blue'>{this.props.problem.tries}</Label>}
+                    {code && <Label as='a' color='blue'>{this.props.problem.tries} submissions</Label>}
                     {evaluation === 'TIME_LIMIT_EXCEEDED' && code && <Label as='a' color='yellow'>TLE</Label>}
                     {evaluation === 'RUNTIME_ERROR' && code && <Label as='a' color='red'>Runtime error</Label>}
                     {(evaluation === 'WRONG_ANSWER' || evaluation === 'EMPTY_ANSWER' || evaluation === 'RUNTIME_ERROR' || evaluation === 'TIME_LIMIT_EXCEEDED') && code && <Label as='a' color='red'>Wrong solution</Label>}
