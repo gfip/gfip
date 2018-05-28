d3809181b41333f46a32ae2f7cfbf32925af5e04
Lucas <lbam@cin.ufpe.br>
2018-05-28 20:02:57 -0300

    Add tries, RTE and TLE indicator

--- client/src/components/CodeViewer.js
+++ client/src/components/CodeViewer.js
@@ -17,7 +17,10 @@ class CodeViewer extends Component {
         return (
             <div className="container column centered report_main">
                 <div className='container row report_label'>
-                    {(evaluation === 'WRONG_ANSWER' || evaluation === 'EMPTY_ANSWER') && code && <Label as='a' color='red'>Wrong solution</Label>}
+                    {code && <Label as='a' color='blue'>{this.props.problem.tries}</Label>}
+                    {evaluation === 'TIME_LIMIT_EXCEEDED' && code && <Label as='a' color='yellow'>TLE</Label>}
+                    {evaluation === 'RUNTIME_ERROR' && code && <Label as='a' color='red'>Runtime error</Label>}
+                    {(evaluation === 'WRONG_ANSWER' || evaluation === 'EMPTY_ANSWER' || evaluation === 'RUNTIME_ERROR' || evaluation === 'TIME_LIMIT_EXCEEDED') && code && <Label as='a' color='red'>Wrong solution</Label>}
                     {evaluation === 'CORRECT' && code && <Label as='a' color='green'>Correct answer</Label>}
                 </div>
                 {evaluation !== 'EMPTY' && code && <SyntaxHighlighter showLineNumbers className='report_code' language='java' style={xcode}>{String(code)}</SyntaxHighlighter>}
