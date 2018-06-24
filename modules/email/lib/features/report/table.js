cdbff14ee11429c487a58dcc9f279edc4b6241b0
Rafael Mota <rma7@cin.ufpe.br>
2018-06-24 18:41:16 -0300

    Fix Email formatation

--- modules/email/lib/features/report/table.js
+++ modules/email/lib/features/report/table.js
@@ -11,16 +11,17 @@ function getStatus(submission) {
 module.exports = (report, student) => {
   const problems = report.submissions.reduce((acc, submission) =>
     `${acc}<li><strong><a href = 'https://www.thehuxley.com/problem/${submission.problem.theHuxleyId}' >${submission.problem.name}:</a></strong> ${getStatus(submission)} <strong>(${submission.score.toFixed(1)})</strong>;
-      <p><pre>${submission.comment || ''}</pre></p>
+      <p><pre style = 'font-size: 12px; color: black;'>${submission.comment || ''}</pre></p>
     </li>
     `, '');
 
-  return `<p>Olá, ${student.name.split(' ')[0]}</p>
+  return `
+  <p>Olá, ${student.name.split(' ')[0]}</p>
   <p> Segue o Feedback da ${report.list.title} : </p>
   <ul style = 'margin:0; padding:0'>
   ${problems}
   </ul>
-  <p><pre>${report.finalComment || ''}</pre></p>
+  <p><pre style = 'font-size: 12px; color: black;' >${report.finalComment || ''}</pre></p>
   <p>Pontuação total <strong>${report.score.toFixed(1)}/${report.list.totalScore.toFixed(1)}</strong></p>
   Responder para ${report.author}@cin.ufpe.br
 `;
