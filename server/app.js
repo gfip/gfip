1dc40a91f64ec9197fe43ee963bfb176427b7ac0
Rafael <rafael.motalves@gmail.com>
2019-01-04 22:45:49 -0300

    Fix build process

--- server/app.js
+++ server/app.js
@@ -41,7 +41,8 @@ app.use('/api/lists/', listRoutes);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+  console.log(__dirname)
+  res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
 });
 
 app.listen(process.env.PORT || 5000, () => console.log('Listening'));
