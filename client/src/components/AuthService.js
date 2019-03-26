267fde646d224ec4d29642c2881e652314e332e5
Lucas Barros <lbam@cin.ufpe.br>
2019-03-26 20:00:47 -0300

    Update AuthService.js

--- client/src/components/AuthService.js
+++ client/src/components/AuthService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import {me} from '../helpers/api';
 
+axios.defaults.baseURL = process.env.REACT_APP_API_ADDRESS || 'https://localhost:5000';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export default class AuthService {
     // Initializing important variables
     constructor() { // React binding stuff
